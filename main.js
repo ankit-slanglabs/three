ff362ea2cb8796c8aeac6d49bad5e27a78cadaca
ankit tiwari <ankittiwari@Ankits-MacBook-Pro.local>
2024-05-03 11:33:49 +0530

    lehsun

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+
+//Renderer + scene + camera
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 10000 );
+
+
+//On window resize
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 
 //Orbit controls
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -13,8 +24,7 @@ controls.update();
 controls.enableZoom = false;
 
 
-
-renderer.setSize( window.innerWidth, window.innerHeight, false );
+renderer.setSize( window.innerWidth, window.innerHeight ); //False make scene alpha transparent
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.SphereGeometry( 4, 96, 48 ); 
@@ -22,14 +32,14 @@ const texture = new THREE.TextureLoader().load("images/earth_atmos_4096.jpg");
 const material = new THREE.MeshLambertMaterial({ map:texture });
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
-sphere.position.x = 0;
-sphere.position.y = 0;
-// sphere.rotation.x += 0.0005;
+// sphere.position.x = 0;
+// sphere.position.y = 0;
+sphere.rotation.z = -50;
 
 
 //bump map
 
-sphere.castShadow = true;
+// sphere.castShadow = true;
 material.wireframe = false;
 
 camera.position.z = 15;
@@ -55,7 +65,7 @@ camera.position.x = 0;
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 scene.add( directionalLight );
 directionalLight.position.set( 60, 15, -30 );
-directionalLight.castShadow = true;
+// directionalLight.castShadow = true;
 
 
 const Ambientlight = new THREE.AmbientLight( 0x404040, .2 );
@@ -67,7 +77,7 @@ function animate() {
 	controls.update();
 
 	// sphere.rotation.x += 0.0001;
-	sphere.rotation.y += 0.001;
+	sphere.rotation.y += 0.002;
 	// sphere.rotation.z += 0.001;
 
 	renderer.render( scene, camera );
